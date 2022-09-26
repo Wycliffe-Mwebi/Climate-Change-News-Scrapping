9ead0e68e3efb4a09eab8f12b0f733ab1494ab40
Wycliffe_Mwebi <wikie7@gmail.com>
2022-09-26 09:02:56 +0300

    Update ❔_About.py

--- Project 4 -Google News Scrapper with Streamlit/pages/❔_About.py
+++ Project 4 -Google News Scrapper with Streamlit/pages/❔_About.py
@@ -9,5 +9,5 @@ st.set_page_config(
 
 st.title("About this App")
 
-st.write("This google news scrapper app allows you to scrape all the news that Google shows based on your input query. This app will enable you to find out about your company's exposure or the information regarding the trend that may catch your eyes.")
+st.write("This google news scrapper app allows you to scrape all the news that Google shows based on your input query. This web app is easy and intuitive to use.")
 
